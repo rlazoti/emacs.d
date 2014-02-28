916d5caaf0b4b1e31494189522a517c826c0bf3c
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2014-02-28 11:38:28 -0300

    Add more folder to be ignored by flipr

--- init.el
+++ init.el
@@ -91,4 +91,4 @@
 (setq fiplr-root-markers '(".git"))
 (global-set-key (kbd "C-x f") 'fiplr-find-file)
 (setq fiplr-ignored-globs '((directories (".git" ".svn" ".ensime_lucene" "target" ".settings"))
-                            (files ("*.class" "*.Jpg" "*.png" "*.zip" "*~"))))
+                            (files ("*.cache" "*.cache#" "*.class" "*.Jpg" "*.png" "*.zip" "*~"))))
