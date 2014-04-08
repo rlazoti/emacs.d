2236d830426273d4d8db0fb48d1e1772ea00c023
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2014-04-08 12:04:00 -0300

    add new modules and core confs

--- modules/emacs-theme.el
+++ modules/emacs-theme.el
@@ -1,5 +1,7 @@
-(emacs-require-package 'color-theme-solarized)
+;;(emacs-require-package 'color-theme-solarized)
+(emacs-require-package 'twilight-theme)
 
-(load-theme 'solarized-dark t)
+;;(load-theme 'solarized-dark t)
+(load-theme 'twilight t)
 
 (provide 'emacs-theme)
