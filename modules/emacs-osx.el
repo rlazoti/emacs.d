caa7c4b3e40ce5dfe5fd2bab14af37791a6174af
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2014-04-17 20:51:23 -0300

    osx stuff

--- modules/emacs-osx.el
+++ modules/emacs-osx.el
@@ -1,9 +1,10 @@
 (set-default-font "Monaco 14")
-;;(setq mac-option-modifier 'meta)
-;;(setq mac-option-key-is-meta nil)
-;;(setq mac-command-key-is-meta nil)
-;;(setq mac-command-modifier 'metafont-mode)
-;;(setq ns-function-modifier 'control)
+
+
+;; use command as meta key
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
 
 ;;add-hook 'term-setup-hook
 ;; '(lambda ()
