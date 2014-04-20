2f591ddbb089d27c4051b8dac9964d106f6930c5
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2014-04-20 18:37:52 -0300

    multiple cursors keys

--- modules/emacs-multiple-cursors.el
+++ modules/emacs-multiple-cursors.el
@@ -1,11 +1,12 @@
-(emacs-require-package 'multiple-cursors)
+(emacs-require-packages '(multiple-cursors))
 
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-x <up>") 'mc/mark-previous-lines)
+(global-set-key (kbd "C-x <down>") 'mc/mark-next-lines)
+(global-set-key (kbd "C-x <left>") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-x <right>") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-x C-x") 'mc/mark-all-like-this)
 
 
 (provide 'emacs-multiple-cursors)
