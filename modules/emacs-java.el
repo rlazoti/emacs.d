8b114572083179775045e1671cc377f23ae42243
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2014-06-20 11:08:13 -0300

    config java mode

--- modules/emacs-java.el
+++ modules/emacs-java.el
@@ -9,6 +9,7 @@
 (require 'malabar-mode)
 ;;(setq malabar-groovy-lib-dir "/path/to/malabar/lib")
 (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+(setq-default tab-width 2)
 
 
 (provide 'emacs-java)
