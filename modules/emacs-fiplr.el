c56c371053d31ce6e945931339574fb72a9fcde0
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2014-08-22 17:36:33 -0300

    ignore ensime cache folder on fiplr search

--- modules/emacs-fiplr.el
+++ modules/emacs-fiplr.el
@@ -4,7 +4,7 @@
 ;; define fiplr for git repositories
 (setq fiplr-root-markers '(".git"))
 (global-set-key (kbd "C-x f") 'fiplr-find-file)
-(setq fiplr-ignored-globs '((directories (".git" ".svn" ".ensime_lucene" "target" ".settings"))
+(setq fiplr-ignored-globs '((directories (".git" ".svn" ".ensime_lucene" "target" ".settings" ".ensime_cache"))
                             (files ("*.cache" "*.cache#" "*.class" "*.Jpg" "*.png" "*.zip" "*~" "*.log"))))
 
 
