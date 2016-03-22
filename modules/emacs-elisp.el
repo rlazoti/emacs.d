dfaad34af06e41db6316117f1b01bb516e296036
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2016-03-21 22:09:39 -0300

    add lisp impl sbcl

--- modules/emacs-elisp.el
+++ modules/emacs-elisp.el
@@ -1,9 +1,12 @@
 (emacs-require-packages '(elisp-slime-nav common-lisp-snippets slime slime-company))
 
 (require 'elisp-slime-nav)
+
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
 	(add-hook hook 'turn-on-elisp-slime-nav-mode))
 
-(slime-setup '(slime-company))
+;(slime-setup '(slime-fancy slime-company))
+(setq slime-contribs '(slime-fancy slime-company))
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
 
 (provide 'emacs-elisp)
