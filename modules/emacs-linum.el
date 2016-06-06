6c8ca2c63275a8cb6c7ff2af76550cfb4232eb74
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-06-06 16:06:44 -0300

    fix linum rendering bug caused by whitespace

--- modules/emacs-linum.el
+++ modules/emacs-linum.el
@@ -10,6 +10,24 @@
                      (list 'help-mode 'shell-mode 'speedbar-mode)))
       (linum-mode 1))))
 
+(eval-after-load 'linum
+  '(progn
+
+     (defface linum-leading-zero
+       `((t :inherit 'linum :foreground "gray7" :background "gray7"))
+       "Face for displaying INVISIBLE leading zeroes for line numbers in display margin."
+       :group 'linum)
+
+     (defun linum-format-func (line)
+       (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+         (concat
+          (propertize (number-to-string 0) 'face 'linum-leading-zero)
+					(propertize (make-string (- w (length (number-to-string line))) ?0) 'face 'linum-leading-zero)
+          (propertize (number-to-string line) 'face 'linum)
+          (propertize (number-to-string 0) 'face 'linum-leading-zero))))
+
+     (setq linum-format 'linum-format-func)))
+
 (custom-linum-mode 1)
 
 (provide 'emacs-linum)
