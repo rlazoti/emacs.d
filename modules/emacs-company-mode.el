ec8fa60d0f4b5cea6ecba72ad1b9d993984397f8
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-06-06 12:49:49 -0300

    change company-mode settings

--- modules/emacs-company-mode.el
+++ modules/emacs-company-mode.el
@@ -1,16 +1,21 @@
-(emacs-require-packages '(company company-inf-ruby))
+(emacs-require-packages '(company company-quickhelp company-web))
 
 (require 'company)
+(require 'company-web-html)
 
-(add-hook 'after-init-hook 'global-company-mode)
+(setq company-echo-delay 0)
 (setq company-idle-delay 0.3)
 (setq company-tooltip-limit 20)
-(setq company-minimum-prefix-length 2)
-(setq company-echo-delay 0)
 (setq company-auto-complete nil)
-(add-to-list 'company-backends 'company-dabbrev t)
-(add-to-list 'company-backends 'company-ispell t)
-(add-to-list 'company-backends 'company-files t)
-(setq company-backends (remove 'company-ropemacs company-backends))
+(setq company-dabbrev-downcase nil)
+(setq company-minimum-prefix-length 2)
+(setq company-tooltip-align-annotations 't) 
+
+(add-to-list 'company-backends 'company-elisp t)
+(add-to-list 'company-backends 'company-yasnippet t)
+(setq company-backends (remove 'company-eclim company-backends))
+
+(add-hook 'after-init-hook 'global-company-mode)
+(company-quickhelp-mode 1)
 
 (provide 'emacs-company-mode)
