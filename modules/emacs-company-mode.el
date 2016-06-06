ae769229d9d1977daa59a0767885dd9712607ffd
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-06-06 13:16:06 -0300

    change company-mode settings

--- modules/emacs-company-mode.el
+++ modules/emacs-company-mode.el
@@ -11,6 +11,19 @@
 (setq company-minimum-prefix-length 2)
 (setq company-tooltip-align-annotations 't) 
 
+(custom-set-faces
+ '(company-preview ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection))))
+ )
+
 (add-to-list 'company-backends 'company-elisp t)
 (add-to-list 'company-backends 'company-yasnippet t)
 (setq company-backends (remove 'company-eclim company-backends))
