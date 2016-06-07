13a574eab55d23990995382ea36bec271f5e5637
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-06-07 17:46:30 -0300

    change js-mode

--- modules/emacs-javascript.el
+++ modules/emacs-javascript.el
@@ -2,16 +2,14 @@
 
 (require 'grunt)
 
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+;; (autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
 
 (setq js-indent-level 2
       js2-indent-level 2
       js2-basic-offset 2
       js2-highlight-level 3)
-
-(setq ac-js2-evaluate-calls t)
 
 (add-hook 'js2-mode-hook 'flymake-mode)
 (add-hook 'js2-mode-hook 'company-mode)
