4099c7d6e8453e1016f4459d0d80cdd612350c14
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-10-03 16:38:17 -0300

    use neotree instead of sr-speedbar

---
+++ modules/emacs-neotree.el
@@ -0,0 +1,8 @@
+(emacs-require-package 'neotree)
+
+(global-set-key [f6] 'neotree-toggle)
+(setq neo-theme 'ascii)
+(setq neo-window-position 'right)
+(setq neo-window-width 40)
+
+(provide 'emacs-neotree)
