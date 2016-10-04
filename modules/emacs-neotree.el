235071819edf2f96517765fbe095e51abb921457
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2016-10-04 18:04:21 -0300

    add neotree refresh key

--- modules/emacs-neotree.el
+++ modules/emacs-neotree.el
@@ -1,6 +1,8 @@
 (emacs-require-package 'neotree)
 
+(global-set-key [f5] 'neotree-refresh)
 (global-set-key [f6] 'neotree-toggle)
+
 (setq neo-theme 'ascii)
 (setq neo-window-position 'right)
 (setq neo-window-width 40)
