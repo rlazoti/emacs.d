fc4bd4749ff688a3c88a61800008a05c8975d8fc
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2016-10-09 10:43:23 -0300

    improve js2-mode conf

--- modules/emacs-javascript.el
+++ modules/emacs-javascript.el
@@ -3,15 +3,17 @@
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-(setq js2-indent-level 2
-      js2-basic-offset 2
-      js2-highlight-level 3)
-
 (add-hook 'json-mode 'flymake-json-load)
-(add-hook 'js2-mode-hook 'flymake-jslint-load)
-(add-hook 'js2-mode-hook 'flymake-mode)
+
 (add-hook 'js2-mode-hook 'company-mode)
-(add-hook 'js2-mode-hook '(lambda ()
-                            (local-set-key (kbd "RET") 'newline-and-indent)))
+(add-hook 'js2-mode-hook 'flymake-mode)
+(add-hook 'js2-mode-hook 'flymake-jslint-load)
+
+(add-hook 'js2-mode-hook (lambda ()
+                          (setq js2-basic-offset 2)
+                          (setq js-indent-level 2)
+                          (setq js2-highlight-level 3)
+                          (setq js2-bounce-indent-p t)
+                          (local-set-key (kbd "RET") 'newline-and-indent)))
 
 (provide 'emacs-javascript)
