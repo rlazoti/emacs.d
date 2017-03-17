f518da7592bbe9dcd5436bc4ba306081809318f2
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2017-03-17 16:06:08 -0300

    decrease neotree font size

--- modules/emacs-neotree.el
+++ modules/emacs-neotree.el
@@ -6,5 +6,12 @@
 (setq neo-theme 'ascii)
 (setq neo-window-position 'right)
 (setq neo-window-width 40)
+(setq neo-smart-open t)
+
+(custom-set-faces
+ '(neo-header-face ((t (:foreground "deep sky blue" :slant normal :weight bold :height 120 :family "Monaco"))))
+ '(neo-root-dir-face ((t (:foreground "deep sky blue" :slant normal :weight bold :height 120 :family "Monaco"))))
+ '(neo-dir-link-face ((t (:foreground "deep sky blue" :slant normal :weight bold :height 120 :family "Monaco"))))
+ '(neo-file-link-face ((t (:foreground "White" :weight normal :height 120 :family "Monaco")))))
 
 (provide 'emacs-neotree)
