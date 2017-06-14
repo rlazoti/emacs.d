7b5db8171f43db2dd82ea3373091eb3f60913204
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2017-06-14 12:54:52 -0300

    improve flipr config

--- modules/emacs-fiplr.el
+++ modules/emacs-fiplr.el
@@ -3,7 +3,7 @@
 ;; define fiplr for git repositories
 (setq fiplr-root-markers '(".git"))
 (global-set-key (kbd "C-x f") 'fiplr-find-file)
-(setq fiplr-ignored-globs '((directories ("bower_components" "node_modules" "dist" ".git" ".svn" ".ensime_lucene" "target" ".settings" ".ensime_cache"))
+(setq fiplr-ignored-globs '((directories ("tmp" "bower_components" "node_modules" "dist" ".git" ".svn" ".ensime_lucene" "target" ".settings" ".ensime_cache"))
                             (files ("*.cache" "*.cache#" "*.class" "*.Jpg" "*.png" "*.zip" "*~" "*.log" ".DS_Store"))))
 
 (provide 'emacs-fiplr)
