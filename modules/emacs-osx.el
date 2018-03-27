d8d35166085e1d0b28297c8f0ed4f751154e9d0f
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2018-03-27 15:39:09 -0300

    change key mapping for macos

--- modules/emacs-osx.el
+++ modules/emacs-osx.el
@@ -1,8 +1,11 @@
 (set-default-font "Monaco 12")
 
-;; use command as meta key
-(setq mac-option-modifier 'super)
-(setq mac-command-modifier 'meta)
+;; set keys for Apple keyboard, for emacs in OS X
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+
+(setq mac-control-modifier 'control)
+(setq ns-function-modifier 'hyper)
 
 ;; macOS command ls doesn't support --dired option
 (setq dired-use-ls-dired nil)
