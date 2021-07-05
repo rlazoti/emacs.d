1a8dc93e44813878bb5228b2fd1ba1c815d1d800
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2021-07-05 11:36:59 -0500

    add graphql module

--- emacs-modules.el
+++ emacs-modules.el
@@ -13,6 +13,7 @@
 (require 'emacs-font)
 (require 'emacs-git-gutter-fringe)
 (require 'emacs-git-timemachine)
+(require 'emacs-graphql)
 (require 'emacs-highlight-symbol)
 (require 'emacs-hydra)
 (require 'emacs-indent-guide)
