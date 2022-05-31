063cb426696fdf6419a4058f1e81d5e57f52e70b
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2022-05-31 18:26:52 -0500

    Disable undo tree backup files

--- modules/emacs-undo-tree.el
+++ modules/emacs-undo-tree.el
@@ -3,6 +3,7 @@
   (global-undo-tree-mode 1)
   (setq undo-tree-visualizer-diff 1)
   (setq undo-tree-visualizer-timestamps 1)
+  (setq undo-tree-auto-save-history nil)
 
   :bind (:map global-map
               ("s-/" . undo-tree-visualize)))
