43178c29efdf6f2666dbd59873911643a51e049d
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2022-12-15 15:27:15 -0600

    change ts-mode identation

--- modules/emacs-typescript.el
+++ modules/emacs-typescript.el
@@ -1,6 +1,7 @@
 (use-package typescript-mode
   :init
   (define-derived-mode typescript-tsx-mode typescript-mode "tsx")
+  (setq typescript-indent-level 2)
   :config
   (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-tsx-mode)))
 
